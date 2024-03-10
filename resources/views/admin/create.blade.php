b5cbce593b6b69ff90e1ff64cf2ac30a89775ef5
Nadirsah <isayevnadirsah26@gmaiL.com>
2024-03-10 16:39:19 +0400

    first commit

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,19 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+
+<x-admin.layouts.card class='w-50 mx-auto'>
+    <x-slot name="title">
+        PHP Laravel Attributes
+    </x-slot>
+    <x-slot name="content">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nulla natus voluptate. Nulla ea est deleniti
+        accusantium obcaecati doloremque hic ad nemo. Distinctio beatae neque, est assumenda obcaecati voluptatum id.
+    </x-slot>
+    <x-slot name="footer">
+        Lorem ipsum dolo.
+    </x-slot>
+</x-admin.layouts.card>
