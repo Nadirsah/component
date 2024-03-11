183d4121c78fb26a1c853d582199c162c58e5042
Nadirsah <isayevnadirsah26@gmaiL.com>
2024-03-11 13:33:47 +0400

    form_fill

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -5,13 +5,44 @@
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
 
-<x-admin.layouts.card class='w-50 mx-auto'>
+<x-admin.layouts.card class='w-50 mx-auto mt-5'>
     <x-slot name="title">
-        PHP Laravel Attributes
+        Elave et
     </x-slot>
     <x-slot name="content">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nulla natus voluptate. Nulla ea est deleniti
-        accusantium obcaecati doloremque hic ad nemo. Distinctio beatae neque, est assumenda obcaecati voluptatum id.
+        <form action="" method="post" class="w-100 text-start">
+            <div class="mt-3">
+                <label for="name">Meqale adi</label>
+                <input type="text" name="name" id="name" placeholder="yazin" class="form-control">
+            </div>
+
+            <div class="mt-3">
+                <label for="category" class="mb-2">Kategoriya</label>
+                <select name="category" id="category" class="form-control">
+                    <option value="-1">Kategoriya secin</option>
+                    <option value="1">PHP</option>
+                    <option value="2">C#</option>
+                </select>
+            </div>
+
+            <div class="mt-3">
+                <label for="content">Meqale adi</label>
+                <textarea name="content" id="content" placeholder="yazin" class="form-control" cols="30"
+                    rows="10"></textarea>
+            </div>
+
+            <div class="mt-3">
+                <input type="checkbox" name="status" id="status" class="me-3">
+                <label for="status" class="mb-2">Meqale gonderilsin?</label>
+            </div>
+
+            <hr>
+
+            <div class="mt-3">
+                <input type="submit" id="btnsave" class="btn btn-success w-100" value="Kaydet">
+            </div>
+
+        </form>
     </x-slot>
     <x-slot name="footer">
         Lorem ipsum dolo.
